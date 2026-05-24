69a79c32415afbe5543c4fed23fd5ac435b6147a
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2026-05-24 14:36:34 -0300

    Add MAIN_BIN variable to make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=export ICON=https://github.com/Flashrom.png
 export DESKTOP=DUMMY
+export MAIN_BIN=flashrom
 
 # Deploy dependencies
 quick-sharun /usr/bin/flashrom
