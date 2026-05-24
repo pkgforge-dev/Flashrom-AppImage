6d272d94d468c267073cafc7ee5861e95e7654ca
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2026-05-24 14:33:55 -0300

    Update AppImage script with new icon and binary path
    Updated ICON to a URL and changed DESKTOP entry to DUMMY. Modified binary path for quick-sharun.

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,11 +8,11 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=export ICON=https://github.com/FFlashrom.png
+export DESKTOP=DUMMY
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/flashrom
 
 # Additional changes can be done in between here
 
