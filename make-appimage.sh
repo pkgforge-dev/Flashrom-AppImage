579ceb99401c5be7c7c0b3037621b7ffac83df65
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2026-05-24 15:25:19 -0300

    Change desktop entry and main binary path

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,8 +9,7 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://github.com/Flashrom.png
-export DESKTOP=flashrom
-export MAIN_BIN=/usr/bin/flashrom
+export DESKTOP=DUMMY
 
 # Deploy dependencies
 quick-sharun /usr/bin/flashrom \
