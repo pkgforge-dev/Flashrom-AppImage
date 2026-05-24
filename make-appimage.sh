7b276db3381d82c561e0329ddc922fd4eaec846a
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2026-05-24 14:51:52 -0300

    Update desktop entry for AppImage to 'flashrom'

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,7 +9,7 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=export ICON=https://github.com/Flashrom.png
-export DESKTOP=DUMMY
+export DESKTOP=flashrom
 export MAIN_BIN=flashrom
 
 # Deploy dependencies
