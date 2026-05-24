27d2c12a2d4211095f682a24e20a8a0b909d3b19
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2026-05-24 15:40:58 -0300

    Update desktop entry and disable AppImage test
    Changed the desktop entry name to 'DUMMY' and commented out the test command.

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,7 +9,7 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://github.com/Flashrom.png
-export DESKTOP=flashrom
+export DESKTOP=DUMMY
 export MAIN_BIN=flashrom
 
 # Deploy dependencies
@@ -23,4 +23,4 @@ quick-sharun --make-appimage
 
 # Test the app for 12 seconds, if the test fails due to the app
 # having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
+#quick-sharun --test ./dist/*.AppImage
