d5767f7c802d8ac00011fea79e065ced7f7bc1cd
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2026-05-24 14:59:49 -0300

    Add libftdi, libusb, and pciutils to dependencies

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm flashrom
+pacman -Syu --noconfirm flashrom libftdi libusb pciutils
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
